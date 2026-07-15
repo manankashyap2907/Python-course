dd8db4a93f496bc012710dc2a88a6c5033c49a33
manankashyap2907 <manankashyaprockstar@gmail.com>
2026-07-15 17:42:43 +0100

    Add files via upload

---
+++ class4.py
@@ -0,0 +1,11 @@
+Tree1 = 10
+Tree2 = 50
+Tree3 = 21
+Tree4 = 600
+Tree5 = 302
+
+sum = Tree1+Tree2+Tree3+Tree4+Tree5
+print("The total number of Tree values are",sum)
+
+average = sum/5
+print("The average of all the trees are", average)
