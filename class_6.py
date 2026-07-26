5275ee9f97bcaed42df4bdc1549cd21b244cc750
manankashyap2907 <manankashyaprockstar@gmail.com>
2026-07-26 17:23:03 +0100

    Add files via upload

---
+++ class_6.py
@@ -0,0 +1,25 @@
+a = 10
+b = 15
+c = 0
+
+if a and b and c:
+    print("All numbers have true boolean value")
+else:
+    print("At least 1 of the numbers have boolean value as true")
+
+
+
+a = 10
+b = 0
+c = -20
+
+if a > 0 or b > 0:
+    print("At least 1 number is greater than 0")
+else:
+    print("There is no number that is more than 0")
+
+
+if b > 0 or c > 0:
+    print("At least 1 of the numbers is greater than 0")
+else:
+    print("No number is greater than 0")
