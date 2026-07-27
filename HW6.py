83971cbe35f202c35afbbaf91d57b80fa76aa3ce
manankashyap2907 <manankashyaprockstar@gmail.com>
2026-07-27 13:36:07 +0100

    Add files via upload

---
+++ HW6.py
@@ -0,0 +1,6 @@
+charecter = input("Enter a character: ")
+
+if (charecter >= 'a' and charecter <= 'z') or (charecter >= 'A' and charecter <= 'Z'):
+    print(charecter, "is an alphabet.")
+else:
+    print(charecter, "is not an alphabet.")
