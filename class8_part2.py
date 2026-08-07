268e5672bce678ccc43c308a7906de8a81a1b586
manankashyap2907 <manankashyaprockstar@gmail.com>
2026-08-07 16:41:06 +0100

    Add files via upload

---
+++ class8_part2.py
@@ -0,0 +1,9 @@
+string = input("Please enter your own string: ")
+
+string2 = ('')
+
+for z in string:
+    string2 = z + string2
+
+print("\ The OG string =", string)
+print("\ The Reversed string =", string2)
