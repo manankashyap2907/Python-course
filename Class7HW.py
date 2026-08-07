db7bbaa40ea53c73bd352bdcf668ea80582e6863
manankashyap2907 <manankashyaprockstar@gmail.com>
2026-08-07 12:21:00 +0100

    Add files via upload

---
+++ Class7HW.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+
+if age >= 10:
+    if age <= 20:
+        print("Age is between 10 and 20 years.")
+    else:
+        print("Age is above 20 years.")
+else:
+    print("Age is below 10 years.")
