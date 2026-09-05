be1cc67d6a3fff82f2b383632872d04897b00820
manankashyap2907 <manankashyaprockstar@gmail.com>
2026-09-05 14:40:03 +0100

    Add files via upload

---
+++ weatherclass13p2.py
@@ -0,0 +1,8 @@
+def weather_condition():
+    print("The wether is really cold and freezing in winter")
+    print("The weather is extremely warm and boiling in summer")
+
+winter = "cold"
+summer = "warm"
+
+weather_condition()
