9e9a15f34fc4b8ff76bfe4fdaecbc8fc23412ac5
manankashyap2907 <manankashyaprockstar@gmail.com>
2026-09-16 18:03:58 +0100

    Add files via upload

---
+++ class17_p2.py
@@ -0,0 +1,15 @@
+for a in range (10):
+    if a % 20 == 0:
+        print("Twist")
+
+    elif a % 15 == 0:
+        pass
+
+    elif a % 5 == 0:
+        print("Fizz")
+
+    elif a % 3 == 0:
+        print("Buzz")
+
+    else:
+        print(a)
